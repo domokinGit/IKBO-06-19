d26ff8cccbafd88619bb564c57afe155edb29d97
Anestazi <56628545+LiaNestazi@users.noreply.github.com>
2021-10-31 19:42:44 +0300

    [Imp] class ControlUserAnswer

--- pr3/control_user_answers.py
+++ pr3/control_user_answers.py
@@ -1,14 +1,29 @@
 
 class ControlUserAnswers:
 
-    def __init__(self):
-        return
+    def __init__(self, answer_true, user_answer):
+        self.answer_true = answer_true
+        self.user_answer = user_answer
         
     def correct_answear(self):
-        return 
+        if self.user_answer == 'подсказка':
+            return True
+        if self.user_answer == 'a':
+            return True
+        if self.user_answer == 'b':
+            return True
+        if self.user_answer == 'c':
+            return True
+        if self.user_answer == 'd':
+            return True 
+        return False
 
     def hint_answear(self):
-        return 
+        if self.user_answer == 'подсказка':
+            return True
+        return False
 
     def right_answear(self):
-        return 
+        if self.user_answer == self.answer_true:
+            return True
+        return False
