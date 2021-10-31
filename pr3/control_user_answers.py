ee5a446cd43c88c51287c58aea853829b167b182
Anestazi <56628545+LiaNestazi@users.noreply.github.com>
2021-10-31 18:44:46 +0300

    [Test] class ControlUserAnswers

---
+++ pr3/control_user_answers.py
@@ -0,0 +1,14 @@
+
+class ControlUserAnswers:
+
+    def __init__(self):
+        return
+        
+    def correct_answear(self):
+        return 
+
+    def hint_answear(self):
+        return 
+
+    def right_answear(self):
+        return 
