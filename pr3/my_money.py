f49f21c9802e07f3a2df0067c0da446edb502322
HeeyWhatsup <70632527+HeeyWhatsup@users.noreply.github.com>
2021-11-01 12:50:32 +0300

    [Test] class MyMoney

---
+++ pr3/my_money.py
@@ -0,0 +1,14 @@
+
+
+class MyMoney:
+    def __init__(self):
+        pass
+
+    def add_money(self):
+        return
+
+    def money_level(self):
+        return
+
+    def money_now(self):
+        return
