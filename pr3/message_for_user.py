c0e617488edb66d9dab45e4cc927d529d4967e12
Diana2027 <56549935+Diana2027@users.noreply.github.com>
2021-11-01 14:50:13 +0300

    [Imp] class Message

--- pr3/message_for_user.py
+++ pr3/message_for_user.py
@@ -1,15 +1,51 @@
 class Message:
     def __init__(self):
-        pass
+        self.correct = False
+        self.right = False
+        self.money = 0
+        self.hint = 'a'
+        self.answer1 = "a"
+        self.answer2 = "b"
+        self.answer3 = "c"
+        self.answer4 = "d"
+        self.true_answer = 'a'
 
-    def message_about_money(self):
-        return
+    def message_about_money(self, money):
+        if not(type(money) == int):
+            raise TypeError
+        self.money = money
+        return 'Ваш счет: ' + str(self.money)
 
-    def message_about_hint(self):
-        return
+    def message_about_hint(self, answer1, answer2, answer3, answer4, true_answer):
+        self.answer1 = answer1
+        self.answer2 = answer2
+        self.answer3 = answer3
+        self.answer4 = answer4
+        self.true_answer = true_answer
+        if self.true_answer in self.answer1:
+            return "Ответ " + self.answer1 + " - неправильный ответ!\n"
+        if self.true_answer in self.answer2:
+            return "Ответ " + self.answer2 + " - неправильный ответ!\n"
+        if self.true_answer in self.answer3:
+            return "Ответ " + self.answer3 + " - неправильный ответ!\n"
+        if self.true_answer in self.answer4:
+            return "Ответ " + self.answer4 + " - неправильный ответ!\n"
+        return "Подсказки не будет!\n"
 
-    def message_about_correct(self):
-        return 
+    def message_about_correct(self, correct):
+        if not(type(correct) == bool):
+            raise TypeError
+        self.correct = correct
+        if correct:
+            return "Правильный ввод"
+        else:
+            return "Неправильный ввод!\n" 
 
-    def message_about_rigth(self):
-        return
+    def message_about_rigth(self, right):
+        if not(type(right) == bool):
+            raise TypeError
+        self.right = right
+        if right:
+            return "Правильный ответ!\n"
+        else:
+            return "Неправильный ответ!\n"
