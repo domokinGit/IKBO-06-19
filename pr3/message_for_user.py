512e45e9f87671667e302453427e02eba06a8639
Diana2027 <56549935+Diana2027@users.noreply.github.com>
2021-11-01 11:59:52 +0300

    [Test] class Message

---
+++ pr3/message_for_user.py
@@ -0,0 +1,17 @@
+
+
+class Message:
+    def __init__(self):
+        pass
+
+    def message_about_money(self):
+        return
+
+    def message_about_hint(self):
+        return
+
+    def message_about_correct(self):
+        return 
+
+    def message_about_rigth(self):
+        return
